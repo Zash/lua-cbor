38be9dbf156de2365a61da93ac88e68f179c878e
Kim Alvefur <zash@zash.se>
2016-05-10 15:03:06 +0200

    cbor.lua: Fix luacheck annotation (Thanks Duane)

--- cbor.lua
+++ cbor.lua
@@ -1,7 +1,7 @@
 -- Concise Binary Object Representation (CBOR)
 -- RFC 7049
 --
--- luacheck: ignore unused argument self
+-- luacheck: ignore 212/self
 
 local function softreq(pkg, field)
 	local ok, mod = pcall(require, pkg);
