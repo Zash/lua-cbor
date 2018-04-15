f1cf28ded4aa96be76022929d064186d82e931bc
Kim Alvefur <zash@zash.se>
2018-04-15 18:08:54 +0200

    cbor.lua: Add sanity checks for struct packing functions

--- cbor.lua
+++ cbor.lua
@@ -40,6 +40,14 @@ local b_rshift = softreq("bit32", "rshift") or softreq("bit", "rshift") or
 	dostring "return function(a,b) return a >> b end" or
 	function (a, b) return m_max(0, m_floor(a / (2 ^ b))); end;
 
+-- sanity check
+if s_pack(">I2", 0) ~= "\0\0" then
+	s_pack = nil;
+end
+if s_unpack(">I2", "\1\2\3\4") ~= 0x102 then
+	s_unpack = nil;
+end
+
 local _ENV = nil; -- luacheck: ignore 211
 
 local encoder = {};
